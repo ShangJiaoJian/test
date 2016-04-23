3dbc40c3d3290159a5fb2e37d2c9548a43dadb51
ShangJiaoJian <1411592757@qq.com>
2016-04-23 12:01:42 +0800

    test1

---
+++ VoiceContral/src/sina/CreAmazing/voice/Weather.java
@@ -0,0 +1,13 @@
+package sina.CreAmazing.voice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Weather extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.weather);
+	}
+}
